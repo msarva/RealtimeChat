2e02dc59acca9331ec702efc0f7d0f15041d1d1e
msarva <159479901+msarva@users.noreply.github.com>
2024-07-17 02:06:59 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Time Chat </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+</head>
